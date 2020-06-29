95593e1a4b4ed77e4f5195c9ca6c5764bb190ba0
Hiroya Yabiku <h_yabiku@atoj.co.jp>
2020-06-29 14:11:28 +0900

    implement editable address

--- src/store/index.js
+++ src/store/index.js
@@ -57,5 +57,6 @@ export default new Vuex.Store({
     userName: state => state.login_user ? state.login_user.displayName : '',
     photoURL: state => state.login_user ? state.login_user.photoURL : '',
     uid: state => state.login_user ? state.login_user.uid : null,
+    getAddressById: state => id => state.addresses.find(address => address.id === id)
   }
 })
