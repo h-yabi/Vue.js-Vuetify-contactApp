ca7831c18912875f58d405a19a28c83a768f3f24
Hiroya Yabiku <h_yabiku@atoj.co.jp>
2020-06-29 14:40:07 +0900

    implement delete address function

--- src/store/index.js
+++ src/store/index.js
@@ -27,6 +27,10 @@ export default new Vuex.Store({
     updateAddress(state, { id, address }) {
       const index = state.addresses.findIndex(address => address.id === id)
       state.addresses[index] = address
+    },
+    deleteAddress(state, { id }) {
+      const index = state.addresses.findIndex(address => address.id === id)
+      state.addresses.splice(index, 1)
     }
   },
   actions: {
@@ -59,6 +63,11 @@ export default new Vuex.Store({
     updateAddress({ getters, commit }, { id, address }) {
       if (getters.uid) firebase.firestore().collection(`users/${getters.uid}/addresses`).doc(id).update(address).then(() => {
         commit('updateAddress', { id, address})
+      })
+    },
+    deleteAddress({ getters, commit }, { id }) {
+      if (getters.uid) firebase.firestore().collection(`users/${getters.uid}/addresses`).doc(id).delete().then(() => {
+        commit('deleteAddress', { id })
       })
     }
   },
