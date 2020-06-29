0502ebb0778bd149775487e283a466c0de8a43c0
Hiroya Yabiku <h_yabiku@atoj.co.jp>
2020-06-29 09:18:52 +0900

    embedded Firebase tag

--- src/main.js
+++ src/main.js
@@ -3,8 +3,27 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import store from "./store"
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAWFBA7Agqvr6I_LNQTHkW5kCjlU7MF6kA",
+  authDomain: "my-address-pj-2a1d4.firebaseapp.com",
+  databaseURL: "https://my-address-pj-2a1d4.firebaseio.com",
+  projectId: "my-address-pj-2a1d4",
+  storageBucket: "my-address-pj-2a1d4.appspot.com",
+  messagingSenderId: "166186606068",
+  appId: "1:166186606068:web:f5da5e5c7f6510c2f16da1",
+  measurementId: "G-EB6CL0XK9R"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
+
 
 new Vue({
   vuetify,
